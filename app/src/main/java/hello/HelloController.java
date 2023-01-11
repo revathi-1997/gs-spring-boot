4dcfc7512433258607a581511e43470c54d80655
BecomeDevops <84931173+BecomeDevops@users.noreply.github.com>
2023-01-11 21:46:15 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot2!";
+        return "Hello from Spring Boot!";
     }   
 }
